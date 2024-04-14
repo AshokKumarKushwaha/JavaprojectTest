ea2b5f6b973f1e5abd4577fc5cf868d8018094b1
Ashok <askumar@symplr.com>
2024-04-14 12:45:00 +0530

    first commit

---
+++ src/ex_13042024/Lab010.java
@@ -0,0 +1,29 @@
+package ex_13042024;
+
+public class Bab010 {
+    public static void main(String[] args) {
+        int age = 30;
+       // Rule number 1: variable name is unique
+       // int age =40;
+
+        //Rule number 2: no - keywords
+       // int class =30;
+       // int static = 45;
+
+        //Rule number 3: A-Z, a-z, $ and startswith
+        int a =45;
+        int Z =45;
+        int $ =45;
+       // int _ = 24;
+        int $_1= 45;
+        //int 1 = 45;
+        //int 1_test = 34;
+       // int 1akk= 45;
+
+        //int age =45;
+        //byte age =45;
+
+
+
+    }
+}

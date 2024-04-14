033cab467b03d49a915d855a421c7134ae95551e
Ashok <askumar@symplr.com>
2024-04-14 12:55:18 +0530

    14th April 2024

--- src/ex_13042024/Lab009.java
+++ src/ex_13042024/Lab009.java
@@ -1,2 +1,13 @@
-package ex_13042024;public class Lab009 {
+package ex_13042024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        System.out.println("printing variables");
+        int age =66;
+        //int age =70; its wrong
+        age =70;
+        age =1;
+
+        System.out.println(age);
+    }
 }
--- src/ex_13042024/Lab010.java
+++ src/ex_13042024/Lab010.java
@@ -1,6 +1,6 @@
 package ex_13042024;
 
-public class Bab010 {
+public class Lab010 {
     public static void main(String[] args) {
         int age = 30;
        // Rule number 1: variable name is unique
@@ -9,11 +9,44 @@ public class Bab010 {
         //Rule number 2: no - keywords
        // int class =30;
        // int static = 45;
+        // data_type variable_name = variable_value(literal)
+
+        // Java ->
+        // Primitive Data Type
+        // byte, short, int, long, char
+        // float, double
+        // boolean - true and false
+
+
+
+        // Non primitive Data Types
+        // String, Arrays, Class
+
+
+        // This name identifier - variable_name
+        // Rule 1- Unique
+        // int age = 54;
+
+        // Rule 2 - No Keywords
+        //int static = 66;
+
+        // Rule 3 - A-Z, a-z, $,_ starts
+        int a = 66;
+        int Z = 66;
+        int $ = 66;
+       // int _ = 66;
+        int $_age = 66;
+        int $_age2 = 66;
+//        int 1name  = 34;
+        // int 123_name  = 34;
+//        int #1  = 34;
+
+//        int age = 65;
+//        byte age = 78;
+//
 
         //Rule number 3: A-Z, a-z, $ and startswith
-        int a =45;
-        int Z =45;
-        int $ =45;
+
        // int _ = 24;
         int $_1= 45;
         //int 1 = 45;
